1f51e808271dd7c717257c35c5f7f641eeeead17
JoDaeChan <daechan476@gmail.com>
2025-04-02 03:38:04 +0900

    refactor: 주문 관리 및 종속성 업데이트
    - 중복 처리 및 주문 병합 개선을 포함하여 성능 및 가독성 최적화
    - 그동안 shipmentBoxId 가 불일치 하던 이유를 찾음.. 부동소수점 ㅂㄷㅂㄷ
    - bigInt를 더 잘 처리 할 수 있도록 새로운 JSON 구문 분석 라이브러리 (`JSON-Bigint`)로 전환

--- src/core/order.service.ts
+++ src/core/order.service.ts
@@ -1,10 +1,11 @@
-import { CoupangOrder, CoupangOrderInfo, CronType } from '@daechanjo/models';
+import { CoupangOrder, CronType } from '@daechanjo/models';
 import { RabbitMQService } from '@daechanjo/rabbitmq';
 import { UtilService } from '@daechanjo/util';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Cron } from '@nestjs/schedule';
 import moment from 'moment';
+import JSONbig from 'json-bigint';
 
 @Injectable()
 export class OrderService {
@@ -14,7 +15,7 @@ export class OrderService {
     private readonly utilService: UtilService,
   ) {}
 
-  @Cron('0 */10 * * * *')
+  // @Cron('0 */10 * * * *')
   async orderCron() {
     const cronId = this.utilService.generateCronId();
     try {
@@ -34,114 +35,6 @@ export class OrderService {
     }
   }
 
-  // async orderManagement(type: string, cronId: string) {
-  //   const today = moment().format('YYYY-MM-DD');
-  //   const yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD');
-  //   // const twoDaysAgo = moment().subtract(2, 'days').format('YYYY-MM-DD');
-  //   // const threeDaysAgo = moment().subtract(3, 'days').format('YYYY-MM-DD');
-  //
-  //   const newOrderProducts: CoupangOrder = await this.rabbitmqService.send(
-  //     'coupang-queue',
-  //     'getCoupangOrderList',
-  //     {
-  //       cronId: cronId,
-  //       type: CronType.ORDER,
-  //       // ACCEPT | INSTRUCT
-  //       status: 'ACCEPT',
-  //       vendorId: this.configService.get<string>('COUPANG_VENDOR_ID')!,
-  //       today: today,
-  //       yesterday: yesterday,
-  //     },
-  //   );
-  //
-  //   if (newOrderProducts.data.length <= 0) {
-  //     console.log(`${type}${cronId}: 주문 데이터가 없습니다.`);
-  //     return;
-  //   }
-  //
-  //   // orderId 별로 개수를 카운트
-  //   const orderCounts = new Map<number, number>();
-  //   newOrderProducts.data.forEach((order: CoupangOrderInfo) => {
-  //     orderCounts.set(order.orderId, (orderCounts.get(order.orderId) || 0) + 1);
-  //   });
-  //
-  //   // 중복된 orderId를 가진 주문들 (개수가 2개 이상인 것들)
-  //   const duplicateOrders: CoupangOrderInfo[] = [];
-  //
-  //   // 데이터 분리
-  //   newOrderProducts.data.forEach((order: CoupangOrderInfo) => {
-  //     if (orderCounts.get(order.orderId)! > 1) {
-  //       duplicateOrders.push(order);
-  //     }
-  //   });
-  //
-  //   if (duplicateOrders.length >= 1) {
-  //     console.log(`${type}${cronId}: 총 ${duplicateOrders.length}개의 중복 주문이 있습니다.`);
-  //     // todo 메일 로직
-  //   }
-  //
-  //   const mergedOrders = this.mergeOrdersByIdAndReceiver(newOrderProducts.data);
-  //
-  //   // 묶음배송아이디
-  //   const shipmentBoxIds = newOrderProducts.data.map((item) => item.shipmentBoxId);
-  //
-  //   // 새로운 모든 주문 상품준비중 처리
-  //   await this.rabbitmqService.emit('coupang-queue', 'putOrderStatus', {
-  //     cronId: cronId,
-  //     type: CronType.ORDER,
-  //     shipmentBoxIds: shipmentBoxIds,
-  //   });
-  //   // await this.rabbitmqService.emit('coupang-queue', 'orderStatusUpdate', {
-  //   //   cronId: cronId,
-  //   //   type: CronType.ORDER,
-  //   // });
-  //
-  //   const result = await this.rabbitmqService.send('onch-queue', 'automaticOrdering', {
-  //     cronId: cronId,
-  //     store: this.configService.get<string>('STORE'),
-  //     newOrderProducts: mergedOrders,
-  //     type: CronType.ORDER,
-  //   });
-  //
-  //   const successOrders = result.data.filter((result: any) => result.status === 'success');
-  //   const failedOrders = result.data.filter((result: any) => result.status === 'failed');
-  //
-  //   if (successOrders.length > 0) {
-  //     try {
-  //       setImmediate(async () => {
-  //         await this.rabbitmqService.emit('mail-queue', 'sendSuccessOrders', {
-  //           result: successOrders,
-  //           store: this.configService.get<string>('STORE'),
-  //         });
-  //       });
-  //       console.log(`${type}${cronId}: 성공 이메일 전송 완료`);
-  //     } catch (error: any) {
-  //       console.error(
-  //         `${CronType.ERROR}${type}${cronId}: 성공 이메일 전송 실패\n`,
-  //         error.response?.data || error.message,
-  //       );
-  //     }
-  //   }
-  //
-  //   if (failedOrders.length > 0) {
-  //     try {
-  //       setImmediate(async () => {
-  //         await this.rabbitmqService.emit('mail-queue', 'sendFailedOrders', {
-  //           result: failedOrders,
-  //           store: this.configService.get<string>('STORE'),
-  //           cronId: cronId,
-  //         });
-  //       });
-  //       console.log(`${type}${cronId}: 실패 이메일 전송 완료`);
-  //     } catch (error: any) {
-  //       console.error(
-  //         `${CronType.ERROR}${type}${cronId}: 실패 이메일 전송 실패\n`,
-  //         error.response?.data || error.message,
-  //       );
-  //     }
-  //   }
-  // }
-
   /**
    * 쿠팡 주문을 관리하는 함수
    *
@@ -160,20 +53,18 @@ export class OrderService {
   async orderManagement(type: string, cronId: string): Promise<void> {
     try {
       // 1. 날짜 설정 및 신규 주문 데이터 가져오기
-      const { newOrderProducts } = await this.fetchNewOrders(type, cronId);
+      const responseData = await this.fetchNewOrders(type, cronId);
+      const newOrders = JSONbig.parse(responseData);
 
       // 주문 데이터가 없으면 종료
-      if (newOrderProducts.data.length <= 0) {
+      if (newOrders.length <= 0) {
         console.log(`${type}${cronId}: 주문 데이터가 없습니다.`);
         return;
       }
 
-      // 2. 중복 주문 확인
-      this.handleDuplicateOrders(newOrderProducts.data, type, cronId);
-
       // 3. 주문 병합 및 상태 업데이트
-      const mergedOrders = await this.mergeOrdersByIdAndReceiver(newOrderProducts.data);
-      await this.updateOrderStatus(newOrderProducts.data, cronId);
+      await this.updateOrderStatus(newOrders as any, cronId);
+      const mergedOrders = await this.mergeOrders(newOrders as any[]);
 
       // 4. 자동 주문 처리
       const result = await this.processAutomaticOrdering(mergedOrders, cronId);
@@ -181,10 +72,7 @@ export class OrderService {
       // 5. 이메일 발송
       await this.sendOrderNotifications(result, type, cronId);
     } catch (error: any) {
-      console.error(
-        `${CronType.ERROR}${type}${cronId}: 주문 관리 중 오류 발생\n`,
-        error.response?.data || error.message,
-      );
+      console.error(`${CronType.ERROR}${type}${cronId}: 주문 관리 중 오류 발생\n`, error);
     }
   }
 
@@ -193,101 +81,102 @@ export class OrderService {
    *
    * @param {string} type - 크론 작업 유형
    * @param {string} cronId - 크론 작업 식별자
-   * @returns {Promise<{newOrderProducts: CoupangOrder}>} 가져온 주문 데이터
+   * @returns {Promise<string>} 가져온 주문 데이터
    * @private
    */
-  private async fetchNewOrders(
-    type: string,
-    cronId: string,
-  ): Promise<{ newOrderProducts: CoupangOrder }> {
-    const today = moment().format('YYYY-MM-DD');
-    const yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD');
+  private async fetchNewOrders(type: string, cronId: string): Promise<string> {
+    // const today = moment().format('YYYY-MM-DD');
+    // const yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD');
 
-    const newOrderProducts: CoupangOrder = await this.rabbitmqService.send(
+    const newOrderProducts: { status: string; data: string } = await this.rabbitmqService.send(
       'coupang-queue',
-      'getCoupangOrderList',
+      'newGetCoupangOrderList',
       {
         cronId: cronId,
-        type: CronType.ORDER,
-        status: 'ACCEPT',
-        vendorId: this.configService.get<string>('COUPANG_VENDOR_ID')!,
-        today: today,
-        yesterday: yesterday,
+        type: type,
       },
     );
 
-    return { newOrderProducts };
+    return newOrderProducts.data;
   }
 
-  /**
-   * 중복된 주문을 확인하고 처리합니다
-   *
-   * @param {CoupangOrderInfo[]} orders - 주문 데이터 배열
-   * @param {string} type - 크론 작업 유형
-   * @param {string} cronId - 크론 작업 식별자
-   * @private
-   */
-  private handleDuplicateOrders(orders: CoupangOrderInfo[], type: string, cronId: string): void {
-    // orderId 별로 개수 카운트
-    const orderCounts = new Map<number, number>();
-    orders.forEach((order: CoupangOrderInfo) => {
-      orderCounts.set(order.orderId, (orderCounts.get(order.orderId) || 0) + 1);
-    });
+  private async mergeOrders(orders: CoupangOrder[]) {
+    const mergedOrders: CoupangOrder[] = [];
 
-    // 중복된 orderId를 가진 주문들 찾기
-    const duplicateOrders: CoupangOrderInfo[] = orders.filter(
-      (order: CoupangOrderInfo) => orderCounts.get(order.orderId)! > 1,
-    );
+    // 이미 처리된 주문의 인덱스를 저장하는 Set
+    const processedIndices = new Set<number>();
 
-    if (duplicateOrders.length >= 1) {
-      console.log(`${type}${cronId}: 총 ${duplicateOrders.length}개의 중복 주문이 있습니다.`);
-      // TODO: 중복 주문에 대한 메일 발송 로직 구현
-    }
-  }
+    // 각 주문에 대해
+    for (let i = 0; i < orders.length; i++) {
+      // 이미 처리된 주문이면 건너뛰기
+      if (processedIndices.has(i)) continue;
 
-  /**
-   * 주문 ID와 수령인 정보를 기준으로 주문을 병합합니다
-   *
-   * @description
-   * 이 함수는 다음과 같은 기준으로 주문을 병합합니다:
-   * - 동일한 주문 ID(orderId)를 가지며
-   * - 동일한 수령인 정보(이름, 주소1, 주소2, 우편번호)를 가진 주문을
-   * 하나의 주문으로 병합하고, 각 주문의 orderItems는 배열에 병합됩니다.
-   *
-   * @param {CoupangOrderInfo[]} orders - 병합할 쿠팡 주문 정보 배열
-   * @returns {Promise<CoupangOrderInfo[]>} 병합된 주문 정보 배열
-   * @private
-   */
-  private async mergeOrdersByIdAndReceiver(
-    orders: CoupangOrderInfo[],
-  ): Promise<CoupangOrderInfo[]> {
-    const mergedOrdersMap = new Map<string, CoupangOrderInfo>();
+      const currentOrder = orders[i];
+      const currentProductCode = currentOrder.items[0].vendorInventoryItemName.split(' ')[0];
 
-    orders.forEach((order) => {
-      const receiverKey = `${order.receiver.name}_${order.receiver.addr1}_${order.receiver.addr2}_${order.receiver.postCode}`;
-      const key = `${order.orderId}_${receiverKey}`;
+      // 현재 주문과 병합할 수 있는 다른 주문들 찾기
+      const ordersToMerge: CoupangOrder[] = [];
 
-      if (mergedOrdersMap.has(key)) {
-        const existingOrder = mergedOrdersMap.get(key)!;
-        // 배열을 any[]로 타입 단언
-        (existingOrder.orderItems as any[]).push(...order.orderItems);
-      } else {
-        mergedOrdersMap.set(key, { ...order });
+      for (let j = i + 1; j < orders.length; j++) {
+        // 이미 처리된 주문이면 건너뛰기
+        if (processedIndices.has(j)) continue;
+
+        const otherOrder = orders[j];
+        const otherProductCode = otherOrder.items[0].vendorInventoryItemName.split(' ')[0];
+
+        // 병합 조건 확인
+        if (
+          currentProductCode === otherProductCode &&
+          currentOrder.memberId === otherOrder.memberId &&
+          currentOrder.memberName === otherOrder.memberName &&
+          currentOrder.addr1 === otherOrder.addr1 &&
+          currentOrder.addr2 === otherOrder.addr2 &&
+          currentOrder.receiverName === otherOrder.receiverName
+        ) {
+          ordersToMerge.push(otherOrder);
+          processedIndices.add(j);
+        }
       }
-    });
 
-    return Array.from(mergedOrdersMap.values());
+      // 병합할 주문이 있으면 병합
+      if (ordersToMerge.length > 0) {
+        // 현재 주문의 복사본 생성
+        const mergedOrder: CoupangOrder = { ...currentOrder };
+
+        // items 배열 복사
+        mergedOrder.items = [...currentOrder.items];
+
+        // 병합할 주문들의 items를 추가
+        for (const orderToMerge of ordersToMerge) {
+          mergedOrder.items.push(...orderToMerge.items);
+        }
+
+        // hasMultipleItems 플래그 업데이트
+        mergedOrder.hasMultipleItems = mergedOrder.items.length > 1;
+
+        // 결과 배열에 추가
+        mergedOrders.push(mergedOrder);
+      } else {
+        // 병합할 주문이 없으면 그대로 추가
+        mergedOrders.push(currentOrder);
+      }
+
+      // 현재 주문을 처리된 것으로 표시
+      processedIndices.add(i);
+    }
+
+    return mergedOrders;
   }
 
   /**
    * 주문 상태를 업데이트합니다
    *
-   * @param {CoupangOrderInfo[]} orders - 주문 데이터 배열
+   * @param {CoupangScrapOrderItem[]} orders - 주문 데이터 배열
    * @param {string} cronId - 크론 작업 식별자
    * @returns {Promise<void>} 작업 완료 시 Promise 반환
    * @private
    */
-  private async updateOrderStatus(orders: CoupangOrderInfo[], cronId: string): Promise<void> {
+  private async updateOrderStatus(orders: CoupangOrder[], cronId: string): Promise<void> {
     // 묶음배송아이디 추출
     const shipmentBoxIds = orders.map((item) => item.shipmentBoxId);
 
@@ -302,20 +191,17 @@ export class OrderService {
   /**
    * 온채널 주문 처리를 진행합니다
    *
-   * @param {any[]} mergedOrders - 병합된 주문 데이터
+   * @param {any[]} orders - 병합된 주문 데이터
    * @param {string} cronId - 크론 작업 식별자
    * @returns {Promise<{data: any[]}>} 자동 주문 처리 결과
    * @private
    */
-  private async processAutomaticOrdering(
-    mergedOrders: any[],
-    cronId: string,
-  ): Promise<{ data: any[] }> {
+  private async processAutomaticOrdering(orders: any[], cronId: string): Promise<{ data: any[] }> {
     return await this.rabbitmqService.send('onch-queue', 'automaticOrdering', {
       cronId: cronId,
-      store: this.configService.get<string>('STORE'),
-      newOrderProducts: mergedOrders,
       type: CronType.ORDER,
+      store: this.configService.get<string>('STORE'),
+      orders: orders,
     });
   }
 
